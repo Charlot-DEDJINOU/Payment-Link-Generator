37a627642b2d924231d25982a8782766b044b338
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-03-13 20:57:06 +0100

    Admin Model

---
+++ app/Models/Admin.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Admin extends Authenticatable
+{
+    use HasFactory;
+    use Notifiable;
+
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
