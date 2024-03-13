980c687a3e74a9cc6a1218632e7934c6993fb6ee
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-03-13 20:53:35 +0100

    layout pages

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <link rel="icon" href="img/logo_yl.png" type="image/x-icon" />
+    <link rel="shortcut icon" href="img/logo_yl.png" type="image/x-icon" />
+    <title>YoupiLab | MoMo Pay</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
+    <link rel="stylesheet" href="{{ asset('css/style.css')}}" />
+</head>
+<body>
