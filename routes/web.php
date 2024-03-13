ede0fa5b0f476c9177285dcdc55c5d3dfdc51a61
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-03-13 20:48:02 +0100

    web routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\TransactionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return redirect('https://youpilab.com');
-});
+Route::get('/login', [AdminController::class, 'getLogin'])->name('admin.getlogin');
+Route::get('/register', [AdminController::class, 'getRegister'])->name('admin.getregister');
+Route::post('/login', [AdminController::class, 'postLogin'])->name('admin.postlogin');
+Route::post('/register', [AdminController::class, 'postRegister'])->name('admin.postregister');
+Route::get('/{id_generate}', [TransactionController::class, 'index'])->name('transaction.confirm');
+Route::put('/{id_generate}', [TransactionController::class, 'checkPayementMethod'])->name('transaction.checkPayementMethod');
