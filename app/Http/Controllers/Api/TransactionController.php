ed1e5b5c13ed09ca27b8076a6e0c7d29c1a63dec
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-03-14 16:47:07 +0100

    TransactioController

--- app/Http/Controllers/Api/TransactionController.php
+++ app/Http/Controllers/Api/TransactionController.php
@@ -2,159 +2,86 @@
 
 namespace App\Http\Controllers\Api;
 
-use FedaPay;
-use FedaPay\Transaction;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Transaction;
+use Carbon\Carbon;
 
 class TransactionController extends Controller
 {
-
-    public function __construct()
+    public function store(Request $request)
     {
-        FedaPay\FedaPay::setApiKey(config('fedapay.secret_key'));
-        FedaPay\FedaPay::setEnvironment(config('fedapay.environment'));
+        $validator = Validator::make($request->all(), [
+            'plateform' => 'required|string',
+            'amount' => 'required|numeric',
+            'type' => 'required|string',
+            'url_callback' => 'nullable|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $transactionId = uniqid('txn_', true);
+
+        $expirationTime = Carbon::now()->addMinutes(60 * 48);
+        $link = route('transaction.confirm', ['id_generate' => $transactionId]);
+
+        Transaction::create([
+            'id_generate' => $transactionId,
+            'plateform' => $request->input('plateform'),
+            'amount' => $request->input('amount'),
+            'type' => $request->input('type'),
+            'expiration_time' => $expirationTime,
+            'payment_link' => $link,
+            'status' => 'pending',
+        ]);
+
+        return response()->json(['confirmation_link' => $link], 200);
     }
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+
+    public function index($id_generate)
     {
-        return 'ok';
-    }
+        $transaction = Transaction::where('id_generate', $id_generate)->first();
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function init_transaction($data)
-    {
-        $data = json_decode($data, true);
-
-        // $data = [
-        //     "firstname"=>"fadel",
-        //     "lastname"=>"fadel",
-        //     "email"=>"fadel@gmail.com",
-        //     "number"=>"61250000",
-        //     "countryCode"=>"bj",
-        //     "description"=>"desc",
-        //     "amount"=>"1000",
-        //     "callback_url"=>"https://youpilab.com",
-        // ];
-        
-
-        try {
-            $transaction = FedaPay\Transaction::create(
-                $this->fedapayTransactionData($data)
-            );
-            $token = $transaction->generateToken();
-            return redirect()->away($token->url);
-        } catch(\Exception $e) {
-			\Illuminate\Support\Facades\Log::info($e->getMessage());
-            return back()->with('error', $e->getMessage());
+        if ($transaction && $transaction->isValid()) {
+            return view('transaction.index')->with('id_generate', $id_generate);
+        } else {
+            return view('transaction.expired', compact('transaction'));
         }
     }
 
-    private function fedapayTransactionData($data){
-	
-        $customer_data = [
-            'firstname' =>$data['firstname'],
-            'lastname' =>$data['lastname'],
-            'email' =>$data['email'],
-           // 'callback' =>$callback_url,
-            'phone_number' => [
-                'number'  => $data['number'],
-                'country' => $data['countryCode']
-            ]
-        ];
+    public function checkPayementMethod(Request $request, $id_generate)
+    {
+        $transaction = Transaction::where('id_generate', $id_generate)->first();
 
-        if($data['callback_url'] == "education"){
-            $callback_url = env('EDUCATION_PAYMENT_CALLBACK_URL').$data['description'];
-        }elseif($data['callback_url'] == "components"){
-			$callback_url = env('COMPONENTS_PAYMENT_CALLBACK_URL').$data['description'];
-		}elseif($data['callback_url'] == "FCHANGES"){
-			$callback_url = env('COINCHANGE_PAYMENT_CALLBACK_URL').$data['description'];
-		}elseif($data['callback_url'] == "dakodo"){
-			$callback_url = env('DAKODO_PAYMENT_CALLBACK_URL').$data['description'];
-		}elseif($data['callback_url'] == "iot"){
-			$callback_url = env('IOT_PAYMENT_CALLBACK_URL').$data['description'];
-		}
-		elseif($data['callback_url'] == "convention"){
-			$callback_url = env('CONVENTION_CALLBACK_URL');
-		}
+        if ($transaction && $transaction->isValid()) {
 
-        return [
-            'description' => $data['description'],
-            'amount' => $data['amount'],
-            'currency' => ['iso' => 'XOF'],
-            'callback_url' => $callback_url,
-            'customer' => $customer_data
-        ];
-    }
+            $validator = Validator::make($request->all(), [
+                'country' => 'required|string',
+                'paymentMethod' => 'required|string|',
+            ]);
 
-    public function callback()
-    {  
-        $transaction_id = $request->input('id');
-        $message = '';
-
-        try {
-            $transaction = FedaPay\Transaction::retrieve($transaction_id);
-            switch($transaction->status) {
-                case 'approved':
-                    $message = 'Transaction approuvée.';
-                    
-                break;
-                case 'canceled':
-                    $message = 'Transaction annulée.';
-                    
-                break;
-                case 'declined':
-                    $message = 'Transaction déclinée.';
-                    
-                break;
+            error_log(json_encode($request->all()));
+    
+            if ($validator->fails()) {
+                return view('transaction.index')->with('error','Veuillez remplir tout les champs');
             }
 
-        } catch(\Exception $e) {
-            $message = $e->getMessage();
+            $transaction->update([
+                'method' => $request->input('paymentMethod'),
+                'country' => $request->input('country')
+            ]);
+
+            if($request->input('paymentMethod') == 'Mobile Money')
+                return view('transaction.requestToPay',compact('transaction'));
+            else
+                return redirect()->away('https://vercel.com/dashboard');
+
+        } else {
+            return view('transaction.expired',compact('transaction'));
         }
-
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
