592df752e182a395b22a066b4583f6a281ea1fac
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-03-14 16:49:14 +0100

    new routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\TransactionController;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,8 +17,17 @@ use App\Http\Controllers\Api\TransactionController;
 */
 
 Route::get('/login', [AdminController::class, 'getLogin'])->name('admin.getlogin');
-Route::get('/register', [AdminController::class, 'getRegister'])->name('admin.getregister');
 Route::post('/login', [AdminController::class, 'postLogin'])->name('admin.postlogin');
-Route::post('/register', [AdminController::class, 'postRegister'])->name('admin.postregister');
+Route::get('/logout', [AdminController::class, 'logout'])->name('admin.logout');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+    Route::get('/dashboard/createtransaction', [DashboardController::class, 'getCreateTransaction'])->name('dashboard.getcreatetransaction');
+    Route::get('dashboard/transaction/{id_generate}', [DashboardController::class, 'transaction'])->name('dashboard.transaction');
+    Route::get('dashboard/refresh/transaction/{id_generate}', [DashboardController::class, 'refreshTransaction'])->name('dashboard.refreshtransaction');
+    Route::post('/dashboard/admin', [AdminController::class, 'postRegister'])->name('admin.postregister');
+    Route::get('/dashboard/admin', [AdminController::class, 'getRegister'])->name('admin.getregister');
+});
+
 Route::get('/{id_generate}', [TransactionController::class, 'index'])->name('transaction.confirm');
 Route::put('/{id_generate}', [TransactionController::class, 'checkPayementMethod'])->name('transaction.checkPayementMethod');
